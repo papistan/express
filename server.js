b75e301217c62f9bf08ae40e151e46a28a29da4f
papistan <jpapisan@gmail.com>
2018-01-17 13:31:31 -0800

    Adds express server and ejs template

---
+++ server.js
@@ -0,0 +1,20 @@
+import config from './config'
+import express from 'express'
+import apiRouter from './api'
+
+const server = express()
+
+server.get('/', (req, res) => {
+	res.render('index')
+})
+
+//sets view engine to be EJS
+server.set('view engine', 'ejs')
+
+// brings in api and allows public folder
+server.use('/api', apiRouter)
+server.use(express.static('public'))
+
+server.listen(config.port, () => {
+	console.info("Listening on port", config.port)
+})
